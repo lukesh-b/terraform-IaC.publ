cebe2622cf0f7016dd5c6f1a002c782708e8a785
lukesh-b <lukesh.b.codes@gmail.com>
2025-08-19 22:09:24 +1000

    SG-P1

---
+++ SG/basic-firewall.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+region = "us-east-1" # enter region of your choice
+}
+
+resource "aws_security_group" "allow_tls" { #can enter any local resource name
+name = "my-firewall"
+description = "Managed from terraform"
+
+}
+  
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" { # enter any local resource name
+security_group_id = aws_security_group.allow_tls.id
+cidr_ipv4 = "0.0.0.0/0"
+from_port = 80 # port range from 80
+ip_protocol = "tcp"
+to_port = 80 # port range to 80
+#here specifying only port 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+security_group_id = aws_security_group.allow_tls_ipv4.id
+cidr_ipv4 = "0.0.0.0/0"
+ip_protocol = "-1" # semantically equivalent to all ports
+
+}
+
