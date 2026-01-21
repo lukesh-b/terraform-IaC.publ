4bba6d0bdee339489ed72c8fe0c861f77a788e83
lukesh-b <lukesh.b.codes@gmail.com>
2026-01-21 05:39:48 -0500

    ec2

---
+++ ec2/basicec2.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
+
+### Outputs
+output "instance_id" {
+  value = aws_instance.ec2.id
+}
+
+output "public_ip" {
+  value = aws_instance.ec2.public_ip
+}
+
