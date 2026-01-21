837a990b2c7c7ef5852c1c9bea44e63b54c8507e
lukesh-b <lukesh.b.codes@gmail.com>
2026-01-21 05:57:24 -0500

    ec2

--- ec2/basic-ec2.tf
+++ ec2/basic-ec2.tf
@@ -14,8 +14,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
-  instance_type = "t2.micro"
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1 or any other choice )
+  instance_type = "t2.micro" # free tier eligible, hence !!
 
   tags = {
     Name = "terraform-ec2"
